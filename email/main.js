f72ba7e42461caa5c07517ada33357f407c9df4f
tommaso-mandorino <tommasomandorino@outlook.it>
2025-05-30 19:22:17 +0200

    feat(email exercise): insert email declaration and user input

---
+++ email/main.js
@@ -0,0 +1,14 @@
+// Guest email list
+const guestEmails = [
+                      'mariorossi@gmail.com',
+                      'francescobianchi@outlook.it',
+                      'mariaverdi@live.it',
+                      'lucagrassi@virgilio.it',
+                      'andreaneri@yahoo.com'
+                    ];
+
+// Ask the user his email
+const userEmail = prompt("Inserisci la tua email:");
+
+// Store user presence in the list starting from a false value
+let userPresent = false;
