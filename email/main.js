48af9fbd5179aa304e21be7236aa5fafee6093b4
tommaso-mandorino <tommasomandorino@outlook.it>
2025-05-30 19:24:03 +0200

    feat(email exercise): process data to check user presence in the email list

--- email/main.js
+++ email/main.js
@@ -12,3 +12,19 @@ const userEmail = prompt("Inserisci la tua email:");
 
 // Store user presence in the list starting from a false value
 let userPresent = false;
+
+// Loop all emails in the list
+for (let i = 0; i < guestEmails.length; i++) {
+
+    // IF the user email is equal to one of the list
+    if (userEmail === guestEmails[i]) {
+
+        // Mark user presence
+        userPresent = true;
+
+        // Exit loop, no need to check other emails
+        break;
+
+    }
+
+}
