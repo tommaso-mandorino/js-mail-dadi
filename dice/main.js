f1f0498641142a380e78eda21ac2b4811b3c0b4f
tommaso-mandorino <tommasomandorino@outlook.it>
2025-06-03 06:53:10 +0200

    feat(dice exercise): add number comparisons with relative output result

--- dice/main.js
+++ dice/main.js
@@ -3,3 +3,24 @@ const userNumber = Math.floor(Math.random() * 6) + 1;
 
 // Generate a random number between 1 and 6 for the computer
 const computerNumber = Math.floor(Math.random() * 6) + 1;
+
+// IF user number is grather than computer number
+if (userNumber > computerNumber) {
+
+    // Show a user win message
+    alert(`Hai VINTO! Il tuo numero ${userNumber} è più grande del numero ${computerNumber} del computer.`);
+
+}
+// ELSE IF user number is less than computer number
+else if (userNumber < computerNumber) {
+
+    // Show a user loss message
+    alert(`Hai PERSO! Il tuo numero ${userNumber} è più piccolo del numero ${computerNumber} del computer.`);
+
+}
+// ELSE numbers are the same
+else {
+
+    // Show a player tie message
+    alert(`NESSUNO dei due ha vinto. Entrambi avete il numero ${userNumber}`);
+}
