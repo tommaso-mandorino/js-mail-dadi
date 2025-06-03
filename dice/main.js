9e4195fd32ff72029e3d0c7ca0dfba63b3028fac
tommaso-mandorino <tommasomandorino@outlook.it>
2025-06-03 06:48:33 +0200

    feat(dice exercise): add user and computer random number generation

---
+++ dice/main.js
@@ -0,0 +1,5 @@
+// Generate a random number between 1 and 6 for the user
+const userNumber = Math.floor(Math.random() * 6) + 1;
+
+// Generate a random number between 1 and 6 for the computer
+const computerNumber = Math.floor(Math.random() * 6) + 1;
